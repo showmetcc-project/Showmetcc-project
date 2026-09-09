2f556952ca297908dea293b5744479f91e9a44f1
joaovincosta <jvincostaa@gmail.com>
2026-09-09 12:33:14 -0300

    ajuste

--- index.php
+++ index.php
@@ -354,10 +354,6 @@
 
   <?php require __DIR__ . '/rodape.php'; ?>
 
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
-
   <!-- Preloader -->
   <div id="preloader"></div>
 
--- rodape.php
+++ rodape.php
@@ -8,8 +8,6 @@
           <span class="verde">Show</span><span class="rosa">Me</span>
         </h4>
         <p>Democratizando o acesso à cultura desde 2026.</p>
-        <p><a href="<?= isset($_SESSION['id_user']) ? 'inicio.php' : 'index.php' ?>">Voltar</a> para o início</p>
-
         <div class="social-links">
           <a href="https://www.instagram.com/showmetcc/" aria-label="Instagram"><i class="bi bi-instagram"></i></a>
           <a href="#" aria-label="X"><i class="bi bi-twitter-x"></i></a>
@@ -67,3 +65,5 @@
 
 <script src="assets/js/contactForm.js" defer></script>
 <?php require_once __DIR__ . '/acessibilidade.php'; ?>
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><iclass="bi bi-arrow-up-short"></i></a>

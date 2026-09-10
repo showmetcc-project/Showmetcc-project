b454de3653494b8124d343a64b84cf28c4a54b93
Lorena Da Silva Rodrigues <159820934+Lorena-silva15@users.noreply.github.com>
2026-09-10 02:57:51 +0000

    teste

--- config/email.example.php
+++ config/email.example.php
@@ -6,7 +6,7 @@ return [
     'usuario' => '',
     'senha' => '',
     'criptografia' => 'tls', // tls, ssl ou vazio para conexão sem criptografia
-    'remetente_email' => '',
+    'remetente_email' => 'showmetcc@gmail.com',
     'remetente_nome' => 'ShowMe',
     'destinatario_email' => '', // Se vazio, será usado o e-mail do remetente
     'destinatario_nome' => 'ShowMe',
